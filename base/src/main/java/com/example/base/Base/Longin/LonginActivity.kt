cc852107d91e168bde65f01db00cf0ffb294e6c4
liuyihui <945131558@qq.com>
2018-11-26 17:33:52 +0800

    更新

---
+++ base/src/main/java/com/example/base/Base/Longin/LonginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.base.Base.Longin
+
+import com.example.base.Base.BaseActivity
+import com.example.base.R
+
+/**
+ * Author by LYH, Email myliuyihui@163.com, Date on 2018/11/26 0026.
+ * PS: Not easy to write code, please indicate.
+ */
+class LonginActivity : BaseActivity() {
+    override fun getlayoutxmlid(): Int {
+            return R.layout.longin_activity;
+    }
+
+    override fun init() {
+
+    }
+
+    override fun initdata() {
+
+    }
+}
---
+++ base/src/main/java/com/example/base/Base/Longin/registered_frame.kt
@@ -0,0 +1,17 @@
+package com.example.base.Base.Longin
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Author by LYH, Email myliuyihui@163.com, Date on 2018/11/26 0026.
+ * PS: Not easy to write code, please indicate.
+ */
+class registered_frame : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
