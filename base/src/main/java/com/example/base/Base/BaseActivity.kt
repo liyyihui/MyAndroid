e8dfe821c013fb3b27dc1eddbfa2b90f175c0246
liuyihui <945131558@qq.com>
2018-12-21 17:35:37 +0800

    处理状态栏

--- base/src/main/java/com/example/base/Base/BaseActivity.kt
+++ base/src/main/java/com/example/base/Base/BaseActivity.kt
@@ -2,15 +2,18 @@ package com.example.base.Base
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.gyf.barlibrary.ImmersionBar
 
 /**
  * Author by LYH, Email myliuyihui@163.com, Date on 2018/11/26 0026.
  * PS: Not easy to write code, please indicate.
  */
 abstract class BaseActivity : AppCompatActivity() {
+    protected lateinit var immersionBar: ImmersionBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getlayoutxmlid())
+        inintbar();
         init();
         initdata();
 
@@ -21,4 +24,14 @@ abstract class BaseActivity : AppCompatActivity() {
     abstract fun  init();
     //初始化数据
     abstract fun initdata();
+    //初始化状态栏
+    open fun inintbar(){
+        immersionBar = ImmersionBar.with(this)
+        immersionBar.init()
+    };
+    override fun onDestroy() {
+        super.onDestroy()
+        immersionBar.destroy()
+    }
+
 }
--- home/src/main/java/com/lyh/www/home/View/Mhome.kt
+++ home/src/main/java/com/lyh/www/home/View/Mhome.kt
@@ -1,25 +1,28 @@
 package com.lyh.www.home.View
 
 
+
 import com.example.base.Base.BaseActivity
 import com.lyh.www.home.R
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.example.base.Base.utils.Util
+import com.gyf.barlibrary.ImmersionBar
 
 
 @Route(path = "/main/home")
 class Mhome : BaseActivity() {
 
-
-
     override fun getlayoutxmlid(): Int {
-         return  R.layout.activity_mhome;
+        return  R.layout.activity_mhome;
     }
 
     override fun init() {
-           Util.log("首页")
+        Util.log("首页")
+        immersionBar.titleBar(R.id.toolbar).init()
         ARouter.getInstance().inject(this);
+
+
     }
 
     override fun initdata() {
@@ -27,4 +30,13 @@ class Mhome : BaseActivity() {
     }
 
 
-}
+
+    }
+
+
+
+
+
+
+
+
